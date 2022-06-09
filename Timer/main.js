d60dccaac6a57bbe5464a92072085a48d527d750
Manuel <103509217+DevManuelB@users.noreply.github.com>
2022-06-09 10:16:02 +0200

    Create main.js

---
+++ Timer/main.js
@@ -0,0 +1,41 @@
+let timer = document.querySelector('.timer');
+let startButton = document.querySelector('.start');
+let stopButton = document.querySelector('.stop');
+
+let h = 0;
+let m = 0;
+let s = 0;
+
+let bool = true
+
+function sleep(milliseconds) {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+startButton.addEventListener('click', () => {
+  async function change() {
+    bool = true;
+    while(bool) {
+      s++;
+      timer.innerHTML = h + ':' + m + ':' + s;
+      await sleep(1000);
+
+      if(s == 60) {
+        s = 0;
+        m++;
+      }
+      if(m == 60) {
+        m = 0;
+        s = 0;
+        h++;
+      }
+    }
+  }
+  change();
+  startButton.disabled = true;
+});
+
+stopButton.addEventListener('click', () => {
+  bool = false;
+  startButton.disabled = false;
+});
